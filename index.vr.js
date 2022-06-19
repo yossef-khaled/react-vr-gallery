aae3ace93903b26d52f0e33cb4a7bb19c8f35dab
yossef-khaled <yossef.k.y333@gmail.com>
2022-06-19 14:12:31 +0200

    Initial commit

---
+++ index.vr.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  AppRegistry,
+  asset,
+  Pano,
+  Text,
+  View,
+} from 'react-vr';
+
+export default function react_vr_gallery(props) {
+    return (
+      <View>
+        <Pano source={asset('chess-world.jpg')}/>
+        <Text
+          style={{
+            backgroundColor: '#777879',
+            fontSize: 0.8,
+            fontWeight: '400',
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.2,
+            paddingRight: 0.2,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            transform: [{translate: [0, 0, -3]}],
+          }}>
+          hello
+        </Text>
+      </View>
+    );
+};
+
+AppRegistry.registerComponent('react_vr_gallery', () => react_vr_gallery);
